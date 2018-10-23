3910a073c4a4dc2446ed228124bdae6531f88b9a
MarC0 <dummyaccx@gmail.com>
2018-10-23 23:10:48 +0200

    Fixed crash in sideloader mod version compare

--- HelperLib/HelperLib.cs
+++ HelperLib/HelperLib.cs
@@ -288,11 +288,24 @@ namespace HelperLib
                 var limit = Math.Max(version.First.Length, version.Second.Length);
                 for (var i = 0; i < limit; i++)
                 {
-                    var first = version.First.ElementAtOrDefault(i);
-                    var second = version.Second.ElementAtOrDefault(i);
-                    var result = first.CompareTo(second);
-                    if (result != 0)
-                        return result;
+                    var first = version.First.ElementAtOrDefault(i) ?? string.Empty;
+                    var second = version.Second.ElementAtOrDefault(i) ?? string.Empty;
+                    try
+                    {
+                        var result = first.CompareTo(second);
+                        if (result != 0)
+                            return result;
+                    }
+                    catch (ArgumentException)
+                    {
+                        if (first is string s1 && second is string s2)
+                        {
+                            // Handle invalid characters in strings by comparing them byte by byte
+                            var result = string.CompareOrdinal(s1, s2);
+                            if (result != 0)
+                                return result;
+                        }
+                    }
                 }
                 return version.First.Length.CompareTo(version.Second.Length);
             }
